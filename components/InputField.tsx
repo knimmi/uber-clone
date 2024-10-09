fbe4c354e9e02ab778b0db87db159cdb3455fa0a
knimmi <knimmiboi@gmail.com>
2024-10-09 15:37:04 +0200

    fixed typo

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,16 +1,20 @@
 import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
-    View,
+  View,
   Text,
 } from "react-native";
 
-const InputField = ({ label, labelStyle }) => (<KeyboardAvoidingView>
+const InputField = ({ label, labelStyle }) => (
+  <KeyboardAvoidingView>
     <TouchableWithoutFeedback>
-        <View className="my-2 w-full">
-            <Text className={`tex-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>{label}</Text>
-        </View>
+      <View className="my-2 w-full">
+        <Text className={`tex-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
+          {label}
+        </Text>
+      </View>
     </TouchableWithoutFeedback>
-</KeyboardAvoidingView>;)
+  </KeyboardAvoidingView>
+);
 
 export default InputField;
