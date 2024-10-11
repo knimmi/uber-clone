a3dfa1ef9d2d3588a05f521765401cbe4421fb13
knimmi <knimmiboi@gmail.com>
2024-10-11 16:55:55 +0200

    made home screen added apis but error

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,7 +1,20 @@
+import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
+import * as Location from "expo-location";
 import RideCard from "@/components/RideCard";
-import { images } from "@/constants";
+import { icons, images } from "@/constants";
+import { useLocationStore } from "@/store";
 import { useUser } from "@clerk/clerk-expo";
-import { FlatList, View, Image, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import {
+  FlatList,
+  View,
+  Image,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const recentRides = [
@@ -111,36 +124,100 @@ const recentRides = [
   },
 ];
 export default function Page() {
+  const { setUserLocation, setDestinationLocation } = useLocationStore;
   const { user } = useUser();
-  const loading = false;
+  const loading = true;
+  const [hasPermission, setHasPermission] = useState(false);
+  const handleSignOut = () => {};
+  const handleDestinationPress = () => {};
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setHasPermission(false);
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      const address = await Location.reverseGeocodeAsync({
+        latitude: location.coords?.latitude!,
+        longitude: location.coords?.longitude!,
+      });
+
+      setUserLocation({
+        latitude: location.coords?.latitude,
+        longitude: location.coords?.longitude,
+        address: `${address[0].name}, ${address[0].region}`,
+      });
+    })();
+  }, []);
 
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
-        // data={recentRides?.slice(0, 5)}
-        data={[]}
-              renderItem={({ item }) => (
-                  <RideCard
-                      ride={item} />}
-            className="px-5"
-            keyboardShouldPersistTraps="handled"
-            contentContainerStyle={{
-              paddingBottom: 100,
-            }}
-            ListEmptyComponent={() => (
-              <View className="flex flex-col items-center justify-center">
-                {!loading ? (
-                  <>
-                    <Image source={images.noResult} />
-                  </>
-                ) : (
-                  <Text>Loading</Text>
-                )}
-              </View>
+        data={recentRides?.slice(0, 5)}
+        // data={[]}
+        renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  clasName="w-40 h-40"
+                  alt="No recent rides"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm">No recent rides found</Text>
+              </>
+            ) : (
+              <ActivityIndicator size="small" color="#000" />
             )}
-          />
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-2xl capitalize font-JakartaExtraBold">
+                Welcome{", "}
+                {user?.firstName ||
+                  user?.emailAddresses[0].emailAddress.split("@")[0]}{" "}
+                👋
+              </Text>
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className="justify-center items-center w-10 h-10 rounded-full bg-white"
+              >
+                <Image source={icons.out} className="w-4 h-4" />
+              </TouchableOpacity>
+            </View>
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle="bg-white shadow-md shadow-neutral-300"
+              handlePress={handleDestinationPress}
+            />
+            <>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Your Current Location
+              </Text>
+              <View className="flex flex-row items-center bg-transparent h-[300px]">
+                <Map />
+              </View>
+            </>
+            <Text className="text-xl font-JakartaBold mt-5 mb-3">
+              Recent Rides
+            </Text>
+          </>
         )}
       />
+      <StatusBar style="dark" />
     </SafeAreaView>
   );
 }
